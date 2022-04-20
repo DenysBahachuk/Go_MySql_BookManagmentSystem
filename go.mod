7e1cbbda2533d12ffdd8e3e308cd0e2f93bd27dd
Denys Bahachuk <bagachukdg@gmail.com>
2022-04-20 18:54:58 +0300

    init commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/DenysBahachuk/Go_MySql_BookManagmentSystem
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0
---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,6 @@
+package routes
+
+import (
+	_ "github.com/DenysBahachuk/Go_MySql_BookManagmentSystem/pkg/controllers"
+	_ "github.com/gorilla/mux"
+)
